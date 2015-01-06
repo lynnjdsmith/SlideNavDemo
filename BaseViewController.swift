0caba8db86bc650913bef2993644f16a40c84a21
Lynn Smith <lynnjdsmith@gmail.com>
2015-01-05 21:09:48 -0600

    Bug fix to hide overlay
    Inelegantly used hidden to hide overlay after you touch on it to close
    the menu.

--- BaseViewController.swift
+++ BaseViewController.swift
@@ -17,14 +17,15 @@ class BaseViewController: UIViewController {
   let screenSize: CGRect = UIScreen.mainScreen().bounds
   let topMenuHeight :CGFloat = 60.0
   var menuOpen = false  
-  var menuWidth = 160
+  var menuWidth :CGFloat = 150.0
   var statusHeight = 20
   
   var mainActivityVC  :MainActivityViewController
   var TwoVC           :TwoViewController
   var ThreeVC         :ThreeViewController
   var FourVC          :FourViewController
-
+  var overlay         :UIView
+  
   required init(coder aDecoder: NSCoder) {
     
     mainActivityVC = MainActivityViewController(nibName: "MainActivityViewController", bundle: nil)
@@ -38,6 +39,8 @@ class BaseViewController: UIViewController {
     
     FourVC = FourViewController(nibName: "FourViewController", bundle: nil)
     FourVC.view.frame = CGRectMake(0, topMenuHeight, screenSize.width, screenSize.height - topMenuHeight)
+
+    overlay = UIView(frame: CGRectMake(menuWidth, topMenuHeight, screenSize.width, screenSize.height - topMenuHeight))
     
     super.init(coder: aDecoder)
   }
@@ -45,20 +48,33 @@ class BaseViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   
+    overlay.backgroundColor=UIColor.clearColor()
+    
+    let recognizer = UITapGestureRecognizer(target: self, action:Selector("closeMenu:"))
+    //recognizer.delegate = self
+    self.overlay.addGestureRecognizer(recognizer)
+    
     // add view controllers with the one you want initially displayed added last
+    self.view.addSubview(overlay)
     self.view.addSubview(TwoVC.view)
     self.view.addSubview(ThreeVC.view)
     self.view.addSubview(FourVC.view)
     self.view.addSubview(mainActivityVC.view)
   }
 
+  func closeMenu(recognizer : UIRotationGestureRecognizer) {
+    menuButtonToggle(self)
+  }
   
   @IBAction func menuButtonToggle(sender: AnyObject) {
     if (menuOpen) { // close
       animateCenterPanelXPosition(targetPosition: 0)
+      overlay.hidden = true     // inelegant. shouldn't need this.
       menuOpen = false
     } else {        // open
-      animateCenterPanelXPosition(targetPosition: 150)
+      animateCenterPanelXPosition(targetPosition: menuWidth)
+      self.view.bringSubviewToFront(overlay)
+      overlay.hidden = false
       menuOpen = true
     }
   }
@@ -79,8 +95,9 @@ class BaseViewController: UIViewController {
   }
   
   @IBAction func FourButtonPressed(sender: AnyObject) {
-    self.view.bringSubviewToFront(FourVC.view)
     self.menuButtonToggle(self)
+    self.view.bringSubviewToFront(FourVC.view)
+
   }
 
   func animateCenterPanelXPosition(#targetPosition: CGFloat, completion: ((Bool) -> Void)! = nil) {
